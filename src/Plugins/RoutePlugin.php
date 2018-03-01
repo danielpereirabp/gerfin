5b3975ebb0960253ae2250e30c15c1b2de889a77
Daniel Pereira <daniel.pereira@iesde.com.br>
2018-03-01 17:48:07 -0300

    Configurando combinador de rotas

--- src/Plugins/RoutePlugin.php
+++ src/Plugins/RoutePlugin.php
@@ -1,9 +1,12 @@
 <?php
-
+declare(strict_types=1);
 namespace GerFin\Plugins;
 
 use Aura\Router\RouterContainer;
 use GerFin\ServiceContainerInterface;
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface;
+use Zend\Diactoros\ServerRequestFactory;
 
 class RoutePlugin implements PluginInterface
 {
@@ -12,13 +15,32 @@ class RoutePlugin implements PluginInterface
         $routerContainer = new RouterContainer();
         /* Registrar as rotas da aplicação */
         $map = $routerContainer->getMap();
-        /* Tem a função de identificar a rota que estão sendo acessada */
+        /* Tem a função de identificar a rota que está sendo acessada */
         $matcher = $routerContainer->getMatcher();
         /* Tem a função de gerar links com base nas rotas registradas */
         $generator = $routerContainer->getGenerator();
+        $request = $this->getRequest();
 
         $container->add('routing', $map);
         $container->add('routing.matcher', $matcher);
         $container->add('routing.generator', $generator);
+        $container->add(RequestInterface::class, $request);
+        
+        $container->addLazy('route', function (ContainerInterface $container) {
+            $matcher = $container->get('routing.matcher');
+            $request = $container->get(RequestInterface::class);
+            return $matcher->match($request);
+        });
+    }
+
+    protected function getRequest(): RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER,
+            $_GET,
+            $_POST,
+            $_COOKIE,
+            $_FILES
+        );
     }
 }
