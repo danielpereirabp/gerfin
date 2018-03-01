4a7d8585aa631f5abd75e8be200d5bc45d3f8d49
Daniel Pereira <daniel.pereira@iesde.com.br>
2018-03-01 14:48:43 -0300

    Criação da classe Application

---
+++ src/Application.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+namespace GerFin;
+
+class Application
+{
+    private $serviceContainer;
+
+    public function __construct(ServiceContainerInterface $serviceContainer)
+    {
+        $this->serviceContainer = $serviceContainer;
+    }
+
+    public function service($name)
+    {
+        return $this->serviceContainer->get($name);
+    }
+
+    public function addService(string $name, $service)
+    {
+        if (\is_callable($service)) {
+            $this->serviceContainer->addLazy($name, $service);
+        } else {
+            $this->serviceContainer->add($name, $service);
+        }
+    }
+}
