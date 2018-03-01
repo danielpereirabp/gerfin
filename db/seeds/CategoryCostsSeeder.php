e82d8adf7c8b264d34ca3cd0044e1cb8b3fe29ec
Daniel Pereira <daniel.pereira@iesde.com.br>
2018-03-01 11:15:54 -0300

    Integrando Faker com seeders

---
+++ db/seeds/CategoryCostsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class CategoryCostsSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create('pt_BR');
+        
+        $data = [];
+        foreach (range(1, 10) as $value) {
+            $data[] = [
+                'name' => $faker->name,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ];
+        }
+        
+        $categoryCosts = $this->table('category_costs');
+        $categoryCosts->insert($data)->save();
+    }
+}
